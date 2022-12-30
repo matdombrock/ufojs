e54297b76368969af2f7d4a82570f82e97136be2
= <matdombrock@gmail.com>
2022-12-30 11:40:27 -0800

    fix status

--- cmds/status.mjs
+++ cmds/status.mjs
@@ -3,11 +3,12 @@ export default {
 	help: 'status https://<url> - Returns the status of a website',
 	async script(args, env, tools){
 		const {axios} = tools;
+		args[0] = args[0] || 'https://example.com';
 		if(!args[0].includes('http')){
 			args[0] = 'https://'+args[0];
 			console.log(args[0]);
 		}
-		const res = await axios.get(args[0] || 'https://example.com');
+		const res = await axios.get(args[0]);
 		console.log(res.status);
 	}
 }
