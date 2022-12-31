1b018d18457006312400155c50afa1abf5b1595c
Mathieu Dombrock <matdombrock@gmail.com>
2022-12-30 23:28:54 -0800

    sort logs and aka

--- cmds/aka.mjs
+++ cmds/aka.mjs
@@ -2,6 +2,6 @@ export default {
 	cmd: 'aka',
 	help: 'aka - Returns a the aka list',
 	async script(args, ufo, tools){
-		console.log(ufo.aka);
+		console.log(ufo.aka.sort());
 	}
 }
--- cmds/cmd.mjs
+++ cmds/cmd.mjs
@@ -2,6 +2,6 @@ export default {
 	cmd: 'cmd',
 	help: 'cmd - Returns a the cmd list',
 	script(args, ufo){
-		console.log(Object.keys(ufo.cmds));
+		console.log(Object.keys(ufo.cmds).sort());
 	}
 }
