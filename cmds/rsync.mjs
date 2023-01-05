c04934dcda6d18caff1a8adbbf78003e131180af
= <matdombrock@gmail.com>
2023-01-05 03:42:54 -0800

    Update rsync and shell commands

--- cmds/rsync.mjs
+++ cmds/rsync.mjs
@@ -1,10 +1,21 @@
 import {$} from 'zx';
+import shell from './shell.mjs';
 export default {
 	cmd: 'rsync',
 	help: 'rsync <args> - Runs rsync in the base shell',
 	async script(args, ufo, tools){
         if(ufo.platform === "win32"){
-            await $`ubuntu run rsync ${args}`;
+            console.log('Switching to ubuntu shell');
+            const shellCache = $.shell;
+            $.shell = ufo.homeDir+'/AppData/Local/Microsoft/WindowsApps/ubuntu';
+            try{
+                await $`rsync ${args}`;
+            }
+            catch(err){
+                console.log(err);
+            }
+            console.log('Switching to default shell');
+            $.shell = shellCache;
         }
         else{
             await $`rsync ${args}`;
--- cmds/shell.mjs
+++ cmds/shell.mjs
@@ -1,8 +1,20 @@
 import {$} from 'zx';
 export default {
 	cmd: 'shell',
-	help: 'shell - Returns the name of your current shell',
-	async script(args){
+	help: 'shell <ps|bash|ubuntu|path> - Sets and/or returns the name of your current shell',
+	async script(args, ufo, tools){
+		if(args[0]==="ps" || args[0]==="powershell"){
+			args[0] = 'C:/WINDOWS/SYSTEM32/WINDOWSPOWERSHELL/V1.0/powershell';
+		}
+		if(args[0]==="bash"){
+			args[0] = 'C:/WINDOWS/SYSTEM32/bash';
+		}
+		if(args[0]==="ubuntu"){
+			args[0] = ufo.homeDir+'/AppData/Local/Microsoft/WindowsApps/ubuntu';
+		}
+		if(args[0]){
+			$.shell = args[0];
+		}
 		console.log($.shell);
 	}
 }
